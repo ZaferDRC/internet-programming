01af0cd0349de91b6ce81ec005ae07bbb53b20a4
ZaferDRC <zaferdrc0@gmail.com>
2022-11-02 14:20:20 +0300

    sildim

--- assignment-4/veriler.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$kullanicilar = [
-    ['kullaniciadi' => 'zafer', 'sifre' => '123'],
-    ['kullaniciadi' => 'ahmet', 'sifre' => '12345'],
-    ['kullaniciadi' => 'mehmet', 'sifre' => '123456'],
-];
-?>
-
--- assignment-5/veriler.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$kullanicilar = [
-    ['kullaniciadi' => 'zafer', 'sifre' => '123'],
-    ['kullaniciadi' => 'ahmet', 'sifre' => '1234'],
-    ['kullaniciadi' => 'mehmet', 'sifre' => '12345'],
-];
-
-?>
